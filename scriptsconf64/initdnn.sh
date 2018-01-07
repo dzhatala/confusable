f6029663fb6589d66048c4fd355b5084a6f002d9
Hatala <dzulqarnaenhatala@gmail.com>
2018-01-07 15:30:39 +0900

    states

--- scriptsconf64/initdnn.sh
+++ scriptsconf64/initdnn.sh
@@ -1,7 +1,9 @@
 mkdir -p protonn/work
 touch protonn/work/NMF
-#cmd="python GenInitDNN.py --structure \"	351X1500X4\"  HTE.dnn.bn protonn/work/NMF"
-cmd="python GenInitDNN.py --structure \"	351X1000X200\"  HTE.dnn.bn protonn/work/NMF"
+cmd="python GenInitDNN.py --structure \"	351X351X9\"  HTE.dnn.bn protonn/work/NMF"
+#cmd="python GenInitDNN.py --structure \"	351X700X4\"  HTE.dnn.bn protonn/work/NMF"
+#cmd="python GenInitDNN.py --structure \"	351X1500X9\"  HTE.dnn.bn protonn/work/NMF"
+#cmd="python GenInitDNN.py --structure \"	351X1000X200\"  HTE.dnn.bn protonn/work/NMF"
 echo $cmd ; eval $cmd;
 #python GenInitDNN.py --quiet --structure "351X$500X4" HTE.dnn.bn protonn/work/NMF
 
---
+++ scriptsconf64/nnall.sh
@@ -0,0 +1,13 @@
+./initdnn.sh
+echo "CTRL+C" ; read
+#./step_1_p65_frame_al.sh
+echo "CTRL+C" ; read
+./step_2_p67.sh
+echo "CTRL+C" ; read
+./step_3_p68.sh
+echo "CTRL+C" ; read
+./step_3_p68_dps.sh
+echo "CTRL+C" ; read
+./step_3_p69_addlayer4.sh
+echo "CTRL+C" ; read
+./step_3_p70_fine_tun.sh
