9ab02e6e5e9957bea3181899db68af9e889c388b
Hatala <dzulqarnaenhatala@gmail.com>
2018-01-08 20:10:54 +0900

    delete_MMF

--- scriptsconf64/step_3_p68_dps.sh
+++ scriptsconf64/step_3_p68_dps.sh
@@ -1,8 +1,11 @@
 source env.sh
 S_SCP=train.scp
 N_SCP=train.scp
+#mlf_aligned="-l LABEL -I train.tri.aligned.mlf"
+mlf_aligned="-l LABEL -I noalign.mlf"
+#mlf_aligned="-l LABEL -I mono.manual.mlf"
 cmd="$HTKTOOLS_DIR/HNTrainSGD -C config.basic -C config.pretrain -H dnn3/init/macros -H dnn3/init/hmmdefs -M dnn3 "
-cmd="$cmd -S $S_SCP -N  $N_SCP -l LABEL 	-I train.tri.aligned.mlf"
+cmd="$cmd -S $S_SCP -N  $N_SCP  	$mlf_aligned"
 cmd="$cmd monophones1	"
 echo $cmd ;eval $cmd
 
--- scriptsconf64/step_3_p70_fine_tun.sh
+++ scriptsconf64/step_3_p70_fine_tun.sh
@@ -1,7 +1,8 @@
 source env.sh
 mkdir -p dnn7.finetune/init
 MMF="-H dnn3/macros -H dnn3/hmmdefs"
+mlf_aligned="-l LABEL -I noalign.mlf"
 cmd="$HTKTOOLS_DIR/HNTrainSGD -C config.basic -C config.finetune $MMF "
-cmd="$cmd -M dnn7.finetune -S train.scp  -l LABEL "
-cmd="$cmd -I train.mono.aligned.mlf monophones1"
+cmd="$cmd -M dnn7.finetune -S train.scp   "
+cmd="$cmd $mlf_aligned monophones1"
 echo $cmd ; eval $cmd
